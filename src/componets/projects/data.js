477009e2cc14c8f05a5cb699baa7a27590d544cf
shiva kumar gattu <gshiva0018@gmail.com>
2025-04-20 08:05:40 +0530

    updated fetcing data

--- src/componets/projects/data.js
+++ src/componets/projects/data.js
@@ -3,22 +3,30 @@ export const projects = [
       id: 1,
       title: "Magic Bus",
       description: "NGO platform for education and skilling in India",
-      technologies: ["React", "Tailwind CSS", "Node.js"],
+      technologies: ["React", "Tailwind CSS"],
       image: "https://i.ibb.co/PMBzHtB/React-App-Google-Chrome-26-01-2024-04-52-55.png",
       link: "https://magicbus.vercel.app/",
-      type: "fullstack"
+      type: "frontend"
     },
     {
       id: 2,
       title: "My Friend Info Solution",
       description: "Information management system with React and MongoDB",
-      technologies: ["React", "MongoDB", "Express"],
+      technologies: ["React", "Javascript","css"],
       image: "https://i.ibb.co/Zf3nwD0/React-App-Google-Chrome-26-01-2024-05-11-09.png",
       link: "https://myfriendinfo.vercel.app/",
-      type: "fullstack"
+      type: "frontend"
     },
     {
-      id: 3,
+      id: 3,  
+      title: "Doctor Appointment",
+      description: "Car rental platform with booking system",
+      technologies: ["","React", "tailwind css"],
+      image: "https://media.istockphoto.com/id/1349375133/photo/handsome-stylish-japanese-businessman-in-glasses-reading-notebook-and-watching-news-on.jpg",
+      link: "https://doctor-appointment-9uf8.vercel.app/",
+      type: "fullstack"
+    },{
+      id: 4 ,
       title: "Self Drive Cars",
       description: "Car rental platform with booking system",
       technologies: ["React", "JavaScript", "CSS"],
@@ -26,4 +34,5 @@ export const projects = [
       link: "#",
       type: "frontend"
     }
+
   ];
