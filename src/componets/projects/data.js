2140a317a92ee8514ff28db0a8113d4c7c600585
shiva kumar gattu <gshiva0018@gmail.com>
2025-04-20 08:50:17 +0530

    update profile

--- src/componets/projects/data.js
+++ src/componets/projects/data.js
@@ -24,7 +24,7 @@ export const projects = [
     {
       id: 4 ,
       title: "Adimn Pannel",
-      description: "Car rental platform with booking system",
+      description: "Doctor Appointment booking system uing MERN Stack",
       technologies: ["MongoDB","Express js", "Node Js","React", "tailwind css"],
       image: "https://github.com/user-attachments/assets/83468738-3598-4d25-bc39-a2dd73a5a230",
       link: "https://doctor-appointment-digy-lclx91j9s-shivakumargattus-projects.vercel.app/admin-dashboard",
@@ -40,5 +40,16 @@ export const projects = [
       type: "frontend",
       gitRepo:"#"
     },
+    {
+      id: 6,
+      title: "Doctor Profile",
+      description: "Authenction Doctor Login credentials Email:shiva@gmail.com, password:12345678 ",
+      technologies: ["React", "Tailwind CSS"],
+      image: "https://github.com/user-attachments/assets/8cc8c405-8287-493f-94c5-e69ce9db6472",
+      link: "https://magicbus.vercel.app/",
+      type: "frontend",
+      gitRepo:"https://github.com/shivakumargattu/magicbus-hyderabad/"
+    },
+    
 
   ];
