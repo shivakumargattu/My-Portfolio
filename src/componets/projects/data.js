6a523effcd27f4c281ff8b11ccfa7a139a86187f
shiva kumar gattu <gshiva0018@gmail.com>
2025-04-23 09:53:58 +0530

    added

--- src/componets/projects/data.js
+++ src/componets/projects/data.js
@@ -1,4 +1,15 @@
 export const projects = [
+  {
+    id: 3,  
+    title: "Doctor Appointment",
+    description: "Doctor Appointment booking system uing MERN Stack",
+    technologies: ["MongoDB","Express js", "Node Js","React", "tailwind css"],
+    image: "https://github.com/user-attachments/assets/080f6bd4-729d-46a7-aac7-7dbc5f8ef812",
+    link: "https://doctor-appointment-9uf8.vercel.app/",
+    type: "fullstack",
+    gitRepo:"https://github.com/shivakumargattu/Doctor-Appointment",
+    
+  },
     {
       id: 1,
       title: "Magic Bus",
@@ -10,18 +21,6 @@ export const projects = [
       gitRepo:"https://github.com/shivakumargattu/magicbus-hyderabad/"
     },
 
-    
-    {
-      id: 3,  
-      title: "Doctor Appointment",
-      description: "Doctor Appointment booking system uing MERN Stack",
-      technologies: ["MongoDB","Express js", "Node Js","React", "tailwind css"],
-      image: "https://github.com/user-attachments/assets/080f6bd4-729d-46a7-aac7-7dbc5f8ef812",
-      link: "https://doctor-appointment-9uf8.vercel.app/",
-      type: "fullstack",
-      gitRepo:"https://github.com/shivakumargattu/Doctor-Appointment",
-      
-    },
 
     {
       id: 14646,
@@ -34,7 +33,16 @@ export const projects = [
       gitRepo:"https://github.com/shivakumargattu/food-order-fullstack/"
     },
     {
-      id:242,
+      id:436534,
+      title: "Employee Management System",
+      description: "Managing employee records,  with curd Oprations built with React.js frontend and Node.js/Express.js backend with MongoDB.",
+      technologies: ["Bootstrap 5","MongoDB","Express js", "Node Js","React"],
+      image: "https://github.com/user-attachments/assets/40051c1e-6f6c-4667-bdd5-21c2f6432ad7",
+      link: "https://ajackus-assignment-peach.vercel.app/",
+      type: "fullstack",
+      gitRepo:"https://github.com/shivakumargattu/food-order-fullstack/"
+    },{
+      id:243535352,
       title: "wordle",
       description: "Wordle with React.js Players have six attempts to guess a five-letter target word",
       technologies: ["React", "Tailwind CSS"],
@@ -43,16 +51,9 @@ export const projects = [
       type: "frontend",
       gitRepo:"https://github.com/shivakumargattu/food-order-fullstack/"
     },
-    {
-      id: 4 ,
-      title: "Adimn Pannel",
-      description: "Doctor Appointment booking system uing MERN Stack",
-      technologies: ["MongoDB","Express js", "Node Js","React", "tailwind css"],
-      image: "https://github.com/user-attachments/assets/83468738-3598-4d25-bc39-a2dd73a5a230",
-      link: "https://doctor-appointment-digy-lclx91j9s-shivakumargattus-projects.vercel.app/admin-dashboard",
-      type: "fullstack",
-      gitRepo:"https://github.com/shivakumargattu/Doctor-Appointment/"
-    },
+    
+
+
     
     {
       id: 2233 ,
@@ -64,14 +65,25 @@ export const projects = [
       type: "frontend",
       gitRepo:"https://github.com/shivakumargattu/expense_tracker/"
     },
+    
     {
-      id: 2,
+      id: 4 ,
+      title: "Adimn Pannel",
+      description: "Doctor Appointment booking system uing MERN Stack",
+      technologies: ["MongoDB","Express js", "Node Js","React", "tailwind css"],
+      image: "https://github.com/user-attachments/assets/83468738-3598-4d25-bc39-a2dd73a5a230",
+      link: "https://doctor-appointment-digy-lclx91j9s-shivakumargattus-projects.vercel.app/admin-dashboard",
+      type: "fullstack",
+      gitRepo:"https://github.com/shivakumargattu/Doctor-Appointment/"
+    },
+    {
+      id: 20000000,
       title: "Organic Vegitabes",
       description: "Register & login JWT token-based with protected routes",
       technologies: ["MongoDB","Express js", "Node Js","React", "tailwind css"],
       image: "https://github.com/user-attachments/assets/00a15c6b-ba96-485c-a63c-e0155fd0c75c",
       link: "https://org-vigtb.vercel.app/",
-      type: "fullStack",
+      type: "fullstack",
       gitRepo:"https://github.com/shivakumargattu/OrgVigtb/"
     },
     {
@@ -82,7 +94,7 @@ export const projects = [
       image: "https://i.ibb.co/Zf3nwD0/React-App-Google-Chrome-26-01-2024-05-11-09.png",
       link: "https://myfriendinfo.vercel.app/",
       type: "frontend",
-      gitRepo:"#"
+      gitRepo:""
     },
     {
       id: 6,
@@ -92,7 +104,18 @@ export const projects = [
       image: "https://github.com/user-attachments/assets/8cc8c405-8287-493f-94c5-e69ce9db6472",
       link: "https://doctor-appointment-digy-lclx91j9s-shivakumargattus-projects.vercel.app/",
       type: "fullstack",
-      gitRepo:"https://github.com/shivakumargattu/magicbus-hyderabad/"
+      gitRepo:"https://github.com/shivakumargattu/Doctor-Appointment"
+    },
+    
+    {
+      id: 6588435985894,
+      title: "vendor management system",
+      description: "A full-featured vendor management system with firm registration, product management, and user administration capabilities",
+      technologies: ["MongoDB","Express js", "Node Js","React", "tailwind css"],
+      image: "https://github.com/user-attachments/assets/d4021e2f-ef12-40da-af6b-63cce6b3d5d3",
+      link: "https://swigg-vendor-dashbord.vercel.app/",
+      type: "fullstack",
+      gitRepo:"https://github.com/shivakumargattu/swigg_vendor_dashbord/"
     },
     
 
